ce78a45fd5eb62d4c8c6d6e2a80403ab7a94e654
Brian Kearns <bdkearns@gmail.com>
2014-10-15 06:59:29 -0400

    enhance client test

--- twspy/test/test_client.py
+++ twspy/test/test_client.py
@@ -1,7 +1,9 @@
 from __future__ import print_function
+import collections
 import inspect
 import os
 import sys
+import time
 
 from twspy.ib.EClientSocket import EClientSocket
 from twspy.ib.EWrapper import EWrapper
@@ -11,21 +13,33 @@ TWS_PORT = int(os.environ.get('TWS_PORT', 7496))
 TWS_CLID = int(os.environ.get('TWS_CLID', 0))
 
 def test_client():
+    functions = {}
     predicate = inspect.ismethod if sys.version_info[0] < 3 else inspect.isfunction
-    functions = [name for name, func in inspect.getmembers(EWrapper, predicate)]
-    functions.append('error')
+    for name, func in inspect.getmembers(EWrapper, predicate):
+        functions[name] = inspect.getargspec(func).args[1:]
+    functions['error'] = ['id', 'errorCode', 'errorMsg']
+
+    seen = {}
 
     class MyWrapper(EWrapper):
-        def make(name):
+        def make(name, spec):
             def default(self, *args):
                 print(name, *args)
+                assert len(spec) == len(args)
+                values = collections.OrderedDict(zip(spec, args))
+                seen[name] = values
             return default
 
-        for name in functions:
-            locals()[name] = make(name)
+        for name, spec in functions.items():
+            locals()[name] = make(name, spec)
 
     con = EClientSocket(MyWrapper())
     con.eConnect(TWS_HOST, TWS_PORT, TWS_CLID)
     assert con.isConnected()
+    for i in range(10):
+        if 'nextValidId' in seen:
+            break
+        time.sleep(0.1)
+    assert seen['nextValidId']['orderId'] > 0
     con.eDisconnect()
     assert not con.isConnected()

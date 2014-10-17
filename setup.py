b7590e2b01aac7b68d02b29114ff04dfb1a19a3e
Brian Kearns <bdkearns@gmail.com>
2014-10-17 19:16:21 -0400

    update setup.py with metadata

--- setup.py
+++ setup.py
@@ -1,5 +1,15 @@
-from setuptools import setup, find_packages
+from setuptools import find_packages, setup
+
 setup(
     name='twspy',
+    version='0.1',
+
+    description='Python API to Interactive Brokers TWS',
+    long_description=open('README.rst').read(),
+    author='Brian Kearns',
+    author_email='bdkearns@gmail.com',
+    url='https://github.com/bdkearns/twspy',
+    license='MIT',
+
     packages=find_packages(exclude=['twspy.ib.cfg']),
 )

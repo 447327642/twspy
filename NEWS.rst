3a581833cc30f8fc0fc7dfed3e470a0c6baebb2d
Brian Kearns <bdkearns@gmail.com>
2014-10-21 22:46:41 -0400

    add NEWS.rst

---
+++ NEWS.rst
@@ -0,0 +1,9 @@
+News
+----
+* master
+
+  * Packaging fixes
+
+* 0.1.0
+
+  * Initial release
--- setup.py
+++ setup.py
@@ -2,12 +2,15 @@ from setuptools import find_packages, setup
 
 exec(open('twspy/_version.py').read())
 
+long_description = '\n'.join([open('README.rst').read(),
+                              open('NEWS.rst').read()])
+
 setup(
     name='twspy',
     version=__version__,
 
     description='Python API to Interactive Brokers TWS',
-    long_description=open('README.rst').read(),
+    long_description=long_description,
     author='Brian Kearns',
     author_email='bdkearns@gmail.com',
     url='https://github.com/bdkearns/twspy',
